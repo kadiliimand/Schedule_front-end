e102b098b806f4eddc834d528ab894c5a579c2e1
Katrin Kutti <katrin.kutti@hotmail.com>
2021-02-08 14:17:13 +0200

    vue1

--- src/Views/Employee.vue
+++ src/Views/Employee.vue
@@ -21,16 +21,33 @@
     </table>
     <h1>Team schedule</h1>
     <table border="1">
-      <th>Date</th>
-      <th>Employee name</th>
-      <tr v-for="column in employee">
-        <td>{{ column.name }}</td>
-      <tr v-for="row in schedule">
-        <td>{{ row.date }}</td>
-        <td>{{ row.start_time }}</td>
-        <td>{{ row.end_time }}</td>
-        <td>{{ row.worked_time }}</td>
-
+      <tr>
+        <th>Date</th>
+        <th>Employee</th>
+      <tr v-for="row in employee">
+        <td>{{ row.name }}</td>
+        <td>
+          <table border="1">
+            <tr>
+              <th>Shift</th>
+              <th>Deposit</th>
+              <th>Withdrawal</th>
+              <th>Transfer</th>
+              <th>From Account number</th>
+              <th>To Account number</th>
+              <th>Timestamp</th>
+            </tr>
+            <tr v-for="historyRow in row.transactionHistoryList">
+              <td>{{ historyRow.transactionId }}</td>
+              <td>{{ historyRow.deposit }}</td>
+              <td>{{ historyRow.withdrawal }}</td>
+              <td>{{ historyRow.transfer }}</td>
+              <td>{{ historyRow.fromAccount }}</td>
+              <td>{{ historyRow.toAccount }}</td>
+              <td>{{ historyRow.localDateTime }}</td>
+            </tr>
+          </table>
+        </td>
       </tr>
     </table>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
+    router,
+    render: function (h) {
+        return h(App)
+    }
 }).$mount('#app')
+
+const token = localStorage.getItem('user-token')
+if (token) {
+    axios.defaults.headers.common['Authorization'] = "Bearer" + token
+}
