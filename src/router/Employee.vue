b9a7becc27219e856047918eb866dca13020ac38
Katrin Kutti <katrin.kutti@hotmail.com>
2021-02-08 14:02:11 +0200

    vue1

---
+++ src/router/Employee.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>My schedule</h1>
+    <table border="1">
+      <tr>
+        <th>Date</th>
+        <th>Start time</th>
+        <th>End time</th>
+        <th>Worked time</th>
+        <th>Overtime <input v-model="overtime" placeholder="overtime"></th>
+        <button v-on:click="saveInHtml()">Submit overtime!</button>
+
+      </tr>
+      <tr v-for="row in schedule">
+        <td>{{ row.date }}</td>
+        <td>{{ row.start_time }}</td>
+        <td>{{ row.end_time }}</td>
+        <td>{{ row.worked_time }}</td>
+
+      </tr>
+    </table>
+    <h1>Team schedule</h1>
+    <table border="1">
+      <th>Date</th>
+      <th>Employee name</th>
+      <tr v-for="column in employee">
+        <td>{{ column.name }}</td>
+      <tr v-for="row in schedule">
+        <td>{{ row.date }}</td>
+        <td>{{ row.start_time }}</td>
+        <td>{{ row.end_time }}</td>
+        <td>{{ row.worked_time }}</td>
+
+      </tr>
+    </table>
+
+  </div>
+
+</template>
---
+++ src/router/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Employee login</h1>
+    Username:<input v-model="userName" placeholder="Username"><br>
+    Password:<input v-model="password" placeholder="Password"><br>
+    <button v-on:click="saveInHtml()">Submit</button>
+    <br>
+    <br>
+    <br>
+    <h1>Employer login</h1>
+    Username:<input v-model="userName" placeholder="Username"><br>
+    Password:<input v-model="password" placeholder="Password"><br>
+    <button v-on:click="saveInHtml()">Submit</button>
+  </div>
+
+</template>
